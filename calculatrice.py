418e89f54d2f0deedd88569a1bd51c5420e410ec
proton-alpha-kplr <proton.alpha@training.kplr.fr>
2023-06-26 12:04:09 +0000

    Ajout de la fonction de division

--- calculatrice.py
+++ calculatrice.py
@@ -7,3 +7,9 @@ def soustraction(a, b):
 def multiplication(a, b):
     return a * b
 
+def division(a, b):
+    if b != 0:
+        return a / b
+    else:
+        return "Erreur : Division par zéro"
+
